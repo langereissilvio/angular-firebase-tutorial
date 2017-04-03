1ee26968e846046d0e53580294415a68829adbe5
langereissilvio <silvio.langereis@sensotec.be>
2017-04-03 15:36:45 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCOlTNqVauVvv33wPwmedKJBGjZn6yJcxs",
+  authDomain: "angular-2-tut-acd3f.firebaseapp.com",
+  databaseURL: "https://angular-2-tut-acd3f.firebaseio.com",
+  projectId: "angular-2-tut-acd3f",
+  storageBucket: "angular-2-tut-acd3f.appspot.com",
+  messagingSenderId: "866085153741"
+};
+
 @NgModule({
   declarations: [
     AppComponent
